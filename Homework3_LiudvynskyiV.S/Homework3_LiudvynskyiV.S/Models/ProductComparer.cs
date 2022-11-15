d4bdb36ed40c803244d9f6d20cba88b063e54ad9
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-15 13:32:00 +0200

    Update ProductComparer.cs

--- Homework3_LiudvynskyiV.S/Homework3_LiudvynskyiV.S/Models/ProductComparer.cs
+++ Homework3_LiudvynskyiV.S/Homework3_LiudvynskyiV.S/Models/ProductComparer.cs
@@ -8,12 +8,12 @@ public class ProductComparer : IComparer
     public int Compare(object? x, object? y)
     {
         if (x is null || y is null || x is not Product || y is not Product)
-        {
+        {// де відбувається перехоплення цього винятку?
             throw new ArgumentException("Incorrect value!");
         }
         var firstProduct = x as Product;
         var secondProduct = y as Product;
-        
+        // Якщо різниця в копійках, буде не добре.
         return (int)(firstProduct.Price - secondProduct.Price);
     }
 }
